a46fa7aa359fb808093bb886d68e4d8a0202edf7
Anshul shivhare <anshulsv14@gmail.com>
2025-03-25 12:44:07 +0530

    responsive

--- client/src/configuration/Config.jsx
+++ client/src/configuration/Config.jsx
@@ -1,6 +1,6 @@
 
 
-// const BASE_URL="https://e-banking-au4h.onrender.com";
-const BASE_URL="http://localhost:8000";
+const BASE_URL="https://e-banking-au4h.onrender.com";
+// const BASE_URL="http://localhost:8000";
 
 export default BASE_URL;
